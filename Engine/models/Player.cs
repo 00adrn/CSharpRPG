3e25a9154e81032939bf4926ebcc002569de94e3
adrian <adrianvez11@gmail.com>
2025-02-17 21:55:38 -0500

    Initial commit

---
+++ Engine/models/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+namespace Engine.models;
+
+public class Player : INotifyPropertyChanged
+{
+    private int _experiencePoints;
+
+    public string name { get; set; }
+    public string characterClass { get; set; }
+    public int hitPoints { get; set; }
+
+    public int experiencePoints
+    {
+        get { return _experiencePoints; }
+        set
+        {
+            _experiencePoints = value;
+            OnPropertyChanged("experiencePoints");
+        }
+    }
+    public int level { get; set; }
+    public int gold { get; set; }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
---
+++ Engine/viewModels/GameSession.cs
@@ -0,0 +1,18 @@
+﻿namespace Engine.viewModels;
+using Engine.models;
+
+public class GameSession
+{
+    public Player currentPlayer { get; set; }
+
+    public GameSession()
+    {
+        currentPlayer = new Player();
+        currentPlayer.name = "Arthur";
+        currentPlayer.characterClass = "Saber";
+        currentPlayer.hitPoints = 100;
+        currentPlayer.experiencePoints = 0;
+        currentPlayer.level = 1;
+        currentPlayer.gold = 100000;
+    }
+}
