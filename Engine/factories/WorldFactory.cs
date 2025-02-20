e97f7a858e20e859f2e14997a42f7dda9e71feb7
00adrn <adrianvez11@gmail.com>
2025-02-20 17:30:01 -0500

    created map of the game and implemented it. still needs sprite for every location. got location sprites working too.

--- Engine/factories/WorldFactory.cs
+++ Engine/factories/WorldFactory.cs
@@ -6,9 +6,19 @@ internal class WorldFactory
     internal World CreateWorld()
     {
         World newWorld = new World();
-        
-        newWorld.AddLocation(0, -1, "Home", "Home, sweet home.", "/WPFUI;component/images/locations/csharptestgamehome.png");
-        newWorld.AddLocation(0, 0, "Spider Forest", "Beware of the creepy crawlies.", "/WPFUI;component/images/locations/csharptestgamespiderforest.png");
+        newWorld.AddLocation(0, -1, "Home", "Home, sweet home!", "/WPFUI;component/images/locations/csharptestgamehome.png");
+        newWorld.AddLocation(0, 0, "Road1", "Road leadinng to Town1", "null");
+        newWorld.AddLocation(0, 1, "Town1", "Town1, home of things!", "null");
+        newWorld.AddLocation(1 , 1, "Cave", "Dark, but full of riches!", "null");
+        newWorld.AddLocation(2, 1, "Road2", "Road leading to Town2", "null");
+        newWorld.AddLocation(3, 1, "Town2", "Town2, home of more things!", "null");
+        newWorld.AddLocation(3, 0, "Mountaintops of Something", "Watch your step!", "null");
+        newWorld.AddLocation(3, -1, "Wolf's Forest", "Dominion of Wolf and wolves!", "null");
+        newWorld.AddLocation(2, -1, "Fields", "Fertile lands with nightly disasters.", "null");
+        newWorld.AddLocation(1, -1, "Farmer's Farm", "Home of the Farmer!", "null");
+        newWorld.AddLocation(0, 2, "Spider Forest", "Beware of the creepy crawlies.", "/WPFUI;component/images/locations/csharptestgamespiderforest.png");
+        newWorld.AddLocation(-1, 0 , "City Gate", "Gate leading to the citadel!", "null");
+        newWorld.AddLocation(-2, 0, "City", "Citadel of the things!", "null");
         return newWorld;
     }
 }
