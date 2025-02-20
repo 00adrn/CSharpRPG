b5bc92c147f7146dd1511a4793a827beb1b1c9fc
adrian <adrianvez11@gmail.com>
2025-02-20 02:06:36 -0500

    added ability to traverse across diferent lcoations. currently only 2 locations exist because i am too tired to create the other locations and their sprites.

---
+++ Engine/models/World.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Engine.models;
+
+public class World
+{
+    private List<Location> _locations = new List<Location>();
+
+    internal void AddLocation(int xCoord, int yCoord, string name, string description, string imageName)
+    {
+        Location loc = new Location();
+        loc.xCoordinate = xCoord;
+        loc.yCoordinate = yCoord;
+        loc.name = name;
+        loc.description = description;
+        loc.imageName = imageName;
+        
+        _locations.Add(loc);
+    }
+
+    public Location LocationAt(int xCoord, int yCoord)
+    {
+        foreach (Location loc in _locations)
+        {
+            if (loc.xCoordinate == xCoord && loc.yCoordinate == yCoord)
+            {
+                return loc;
+            }
+        }
+        return null;
+    }
+}
--- Engine/viewModels/GameSession.cs
+++ Engine/viewModels/GameSession.cs
@@ -1,10 +1,57 @@
-﻿namespace Engine.viewModels;
-using Engine.models;
+﻿using System.ComponentModel;
 
-public class GameSession
+namespace Engine.viewModels;
+using Engine.models;
+using Engine.factories;
+
+public class GameSession:INotifyPropertyChanged
 {
+    private Location _currentLocation;
     public Player currentPlayer { get; set; }
-    public Location currentLocation { get; set; }
+    public World currentWorld { get; set; }
+
+    public Location currentLocation
+    {
+        get { return _currentLocation; }
+        set
+        {
+            _currentLocation = value;
+            OnPropertyChanged("currentLocation");
+            OnPropertyChanged("hasLocationNorth");
+            OnPropertyChanged("hasLocationWest");
+            OnPropertyChanged("hasLocationEast");
+            OnPropertyChanged("hasLocationSouth");
+        }
+    }
+
+    public bool hasLocationNorth
+    {
+        get
+        {
+            return currentWorld.LocationAt(currentLocation.xCoordinate, currentLocation.yCoordinate + 1) != null;
+        }
+    }
+    public bool hasLocationWest
+    {
+        get
+        {
+            return currentWorld.LocationAt(currentLocation.xCoordinate - 1, currentLocation.yCoordinate) != null;
+        }
+    }
+    public bool hasLocationEast
+    {
+        get
+        {
+            return currentWorld.LocationAt(currentLocation.xCoordinate + 1, currentLocation.yCoordinate) != null;
+        }
+    }
+    public bool hasLocationSouth
+    {
+        get
+        {
+            return currentWorld.LocationAt(currentLocation.xCoordinate, currentLocation.yCoordinate - 1) != null;
+        }
+    }
 
     public GameSession()
     {
@@ -16,11 +63,32 @@ public class GameSession
         currentPlayer.level = 1;
         currentPlayer.gold = 100000;
         
-        currentLocation = new Location();
-        currentLocation.name = "Home";
-        currentLocation.xCoordinate = 0;
-        currentLocation.yCoordinate = 0;
-        currentLocation.description = "Home, sweet home.";
-        currentLocation.imageName = "Engine/images/locations/Home.png";
+        WorldFactory factory = new WorldFactory();
+        currentWorld = factory.CreateWorld();
+
+        currentLocation = currentWorld.LocationAt(0, -1);
+    }
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+    public void MoveDirection(string direction)
+    {
+        switch (direction)
+        {
+            case "north":
+                currentLocation = currentWorld.LocationAt(currentLocation.xCoordinate, currentLocation.yCoordinate+1);
+                break;
+            case "east":
+                currentLocation = currentWorld.LocationAt(currentLocation.xCoordinate+1, currentLocation.yCoordinate);
+                break;
+            case "south":
+                currentLocation = currentWorld.LocationAt(currentLocation.xCoordinate, currentLocation.yCoordinate-1);
+                break;
+            case "west":
+                currentLocation = currentWorld.LocationAt(currentLocation.xCoordinate-1, currentLocation.yCoordinate);
+                break;
+        }
     }
 }
